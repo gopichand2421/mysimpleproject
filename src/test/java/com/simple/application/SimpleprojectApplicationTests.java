9ab290caf269e612eed88d257a75b9bcdd1c3db4
gopichand2421 <gopichand2416@gmail.com>
2021-07-10 19:03:20 +0530

    simple project commit

---
+++ src/test/java/com/simple/application/SimpleprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
